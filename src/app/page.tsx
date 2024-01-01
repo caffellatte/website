561ac12118dfb9d553499d6c14af484e9bb126ce
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-01-01 22:59:16 +0300

    Update page.tsx
    * Implement Typography component for Text.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import { Typography } from "@/components/ui/base/Text";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-12">
       <div className="flex flex-col gap-3 text-center">
-        <h1 className="text-xl font-medium">Mikhail Lutsenko</h1>
-        <h5 className="text-base">Full-Stack Web Developer</h5>
+        <Typography variant="h4">Mikhail Lutsenko</Typography>
+        <Typography variant="h6">Full-Stack Web Developer</Typography>
         <Link
           href="https://github.com/caffellatte"
           className="underline underline-offset-2 hover:no-underline transition-all duration-200 ease-in-out"
