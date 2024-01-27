bf40d955d1e67aaf1c73dc09d2e5a3c678bc193c
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-01-27 23:53:15 +0300

    Add Server Side Calls Link Creation
    * Create `createCallerFactory` in tRPC Service (apps/server/src/trpc/trpc.service.ts);
    * Import `TrpcRouter` & `TrpcService` in LinksProcessor for creating server side call (apps/server/src/links/links.processor.ts);
    * Import `TrpcRouter` & `TrpcService` as providers in LinksModule (apps/server/src/links/links.module.ts).

--- apps/server/src/links/links.processor.ts
+++ apps/server/src/links/links.processor.ts
@@ -4,20 +4,35 @@ import { Job } from 'bull';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Link } from '@server/links/link.entity';
+import { TrpcService } from '@server/trpc/trpc.service';
+import { TrpcRouter } from '@server/trpc/trpc.router';
 
 @Processor('links')
 export class LinksProcessor {
   constructor(
     @InjectRepository(Link)
     private linksRepository: Repository<Link>,
+    private readonly trpcRouter: TrpcRouter,
+    private readonly trpcService: TrpcService,
   ) {}
   private readonly logger = new Logger(LinksProcessor.name);
+
+  createCaller = this.trpcService.createCallerFactory(
+    this.trpcRouter.appRouter,
+  );
+
+  caller = this.createCaller({});
 
   @Process('analyze')
   async handleAnalyze(job: Job) {
     this.logger.debug('Start Analyze...');
     this.logger.debug(job.data);
     const links = await this.linksRepository.find();
+    this.caller.linkCreate({
+      title: 'title server side created',
+      description: 'description server side created',
+      url: 'url server side created',
+    });
     this.logger.debug(links);
     this.logger.debug('FindOne Analyze');
   }
