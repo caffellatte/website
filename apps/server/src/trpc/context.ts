d302d26fe25c231acae3a06498b5017ec172796f
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-01-28 23:46:00 +0300

    Add tRPC middleware
    * Export middleware from TrpcService (apps/server/src/trpc/trpc.service.ts);
    * Add `isAuthed` middleware to tRPC router (apps/server/src/trpc/trpc.router.ts);
    * Update `createContext` (apps/server/src/trpc/context.ts).

--- apps/server/src/trpc/context.ts
+++ apps/server/src/trpc/context.ts
@@ -1,30 +1,10 @@
-import * as trpcNext from '@trpc/server/adapters/next';
+import { inferAsyncReturnType } from '@trpc/server';
+import { CreateNextContextOptions } from '@trpc/server/adapters/next';
 
-const decodeAndVerifyJwtToken = (token: string): number | null => {
-  if (token === 'ok') {
-    return 1;
-  }
-  return null;
-};
-
-export async function createContext({
-  req,
-}: trpcNext.CreateNextContextOptions) {
-  // Create your context based on the request object
-  // Will be available as `ctx` in all your resolvers
-  // This is just an example of something you might want to do in your ctx fn
-  async function getUserFromHeader() {
-    if (req.headers.authorization) {
-      const user = await decodeAndVerifyJwtToken(
-        req.headers.authorization.split(' ')[1],
-      );
-      return user;
-    }
-    return null;
-  }
-  const user = await getUserFromHeader();
+export async function createContext({ req }: CreateNextContextOptions) {
   return {
-    user,
+    auth: req.headers.authorization === 'ABC',
   };
 }
-export type Context = Awaited<ReturnType<typeof createContext>>;
+
+export type Context = inferAsyncReturnType<typeof createContext>;
