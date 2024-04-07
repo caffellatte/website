da8b07441d1c696dc6b3b36cfb3ab58bbc1d9655
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2024-04-07 22:01:49 +0300

    Update `Register` page & `useRegister` hook
    * Update `useRegister` hook: save tokens to Cookies (apps/client/src/services/hooks/useRegister.ts);
    * Update `Register` page: redirect to `Home` page after register (apps/client/src/components/views/register/Register/Register.tsx).

--- apps/client/src/services/hooks/useRegister.ts
+++ apps/client/src/services/hooks/useRegister.ts
@@ -4,6 +4,7 @@ import {
   type RouterInputs,
   type RouterOutputs,
 } from "@client/services/trpc";
+import Cookies from "js-cookie";
 
 type RegisterOptions = ReactQueryOptions["auth"]["register"];
 
@@ -17,6 +18,10 @@ export function useRegister(options?: RegisterOptions) {
       // when a new link is created
       // utils.linkCreate.invalidate();
       // utils.linksFindAll.invalidate();
+      if (data?.access_token && data?.refresh_token) {
+        Cookies.set("access_token", data.access_token, { expires: 7 });
+        Cookies.set("refresh_token", data.refresh_token, { expires: 7 });
+      }
       utils.invalidate();
       options?.onSuccess?.(data, variables, context);
     },
